10b3e114d37552afd8d5e55d08745f470e23dc4d
cristian3103 <cristiansalazar3103@gmail.com>
2023-05-08 19:09:24 -0500

    se modifico package.json

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const path = require("path");
 
 app.use(express.static("public"));
 
+const port = process.env.PORT || 3000;
+app.listen(port,()=>console.log('servidor corriendo en el puerto ${port}'));
 
-app.listen(process.env.PORT || 3000, () => console.log("SERVIDOR FUNCIONANDO EN EL PUERTO 3000 "));
+//app.listen(process.env.PORT || 3000, () => console.log("SERVIDOR FUNCIONANDO EN EL PUERTO 3000 "));
 
 app.get('/', (req, res) =>{
     res.sendFile(path.resolve(__dirname, './views/home.html'))
